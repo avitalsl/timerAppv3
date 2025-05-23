320c07a0a2ecb467352808a101ae61e29c18bc95
Avital Sloma <avital.sloma@AvitalSlomaMBP2.local>
2025-05-23 13:30:19 +0300

    adding card to kickoff

--- newStandApp/src/components/KickoffScreen.tsx
+++ newStandApp/src/components/KickoffScreen.tsx
@@ -4,7 +4,11 @@ import React from 'react';
  * KickoffScreen - Empty component placeholder
  */
 const KickoffScreen: React.FC = () => {
-  return <div data-testid="kickoff-screen"></div>;
+  return (
+    <div className="bg-white rounded-lg shadow-md p-6" data-testid="kickoff-screen-card">
+      {/* Kickoff content goes here */}
+    </div>
+  );
 };
 
 export default KickoffScreen;
